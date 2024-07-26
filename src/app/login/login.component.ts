3b232d38116ced38128a9cea5fe48e2ae5e2c07a
diogo.andrade <diogo.cesar.cel@gmail.com>
2024-07-26 17:49:15 -0300

    Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
+
 export class LoginComponent {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  readonly senha = new FormControl('', [Validators.required]);
+  hide = signal(true);
+  errorMessage = signal('');
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('Eimail')) {
+      this.errorMessage.set('E-mail obrigatório');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('E-mail inválido');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -11,7 +16,13 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     CommonModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class LoginModule { }
